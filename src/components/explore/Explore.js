29bd445b2a2a686e5f36e7245939b25e90fe8827
Morsalin <morsalinkausar043@gmail.com>
2022-01-05 21:48:18 +0600

    home and explore card desing

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -15,8 +15,8 @@ const Explore = () => {
             {
             isLoading ?
             <div className="col-10 mx-auto col-md-4 text-center py-5">
-                    <div class="spinner-border text-danger" role="status">
-                        <span class="visually-hidden">Loading...</span>
+                    <div className="spinner-border text-danger" role="status">
+                        <span className="visually-hidden">Loading...</span>
                     </div>
                 </div>
                 :
@@ -27,15 +27,15 @@ const Explore = () => {
                                 const {_id , title , body , src , price} = crrElm;
                             
                                 return(
-                                    <div key={_id} class="col">
-                                        <div class="card service_img_box h-100">
-                                            <img src={src} class="services_images" alt="services_image"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title">{title}</h5>
-                                            <p class="card-text">{body.slice(0, 200)}</p>
+                                    <div key={_id} className="col">
+                                        <div className="card service_img_box h-100 border-0 shadow">
+                                            <img src={src} className="services_images" alt="services_image"/>
+                                        <div className="card-body">
+                                            <h5 className="service_title">{title}</h5>
+                                            <p className="service_text">{body.slice(0, 200)}</p>
                                             <div className="d-flex justify-content-around">
                                                 <NavLink to={`explore/${_id}`} className="btn btn-outline-danger">Booking Now</NavLink>
-                                                <h4>Price : ${price}</h4>
+                                                <h4 className='service_text'>Price : ${price}</h4>
                                             </div>
                                         </div>
                                     </div>
